bfddb0f605d7a24ee7497225c71211a1e45c6f28
Nowbie <33182389+NowDev@users.noreply.github.com>
2019-04-21 19:14:13 -0300

    preparando tudo...

---
+++ src/com/orionis/typingmachine/Main.java
@@ -0,0 +1,22 @@
+
+/**
+ *   Package principal da aplicação
+ */
+
+package com.orionis.typingmachine;
+
+/**
+ * @author Orionis Team
+ *    Para fins de aprendizado!
+ */
+
+// Classe principal e inicial da aplicação.
+public class Main {
+
+	//Primeira função, que dará início a tudo...
+	public static void main(String[] args) {
+		
+
+	}
+
+}
